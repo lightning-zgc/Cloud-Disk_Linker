ecdee5403edada97d4c75d5f88c87aeb0fc7d95e
Lightning <lightning-zgc@hotmail.com>
2016-05-14 19:29:20 +0800

    hi

--- upload.php
+++ upload.php
@@ -3,11 +3,12 @@ $approot = '.';
 require_once('function.php');
 $file = @$_POST['file'];
 $start = @$_POST['start'];
-if(!$file){
+$md5 = @$_POST['md5'];
+if(!$md5 and !$file){
   echo json_encode(array('code' => '0', 'msg' => 'void'));
   exit;
 }
-$md5 = md5($file);
+$md5 = $md5?$md5:md5($file);
 $link = sqls();
 $is=0;
 $sql = "select * from upload where md5='$md5' order by id desc limit 1";
@@ -21,6 +22,8 @@ $sql = "select * from upload where md5='$md5' order by id desc limit 1";
   }
 if($is){
   echo json_encode(array('code' => '2', 'msg' => 'Already In'));
+}elseif (!$is and !$file) {
+  echo json_encode(array('code' => '3', 'msg' => 'Null, please upload'));
 }else {
   $sql = "insert into upload(md5,nr,start) values('$md5','$file','$start')";
   	$ret = mysql_query($sql, $link);
